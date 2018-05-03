da7bcba3403a4b35143195661291d51aafd70c34
snakamu <ping.pong.spanish@gmail.com>
2018-05-04 05:10:56 +0900

    sw.js 改善

--- sw.js
+++ sw.js
@@ -26,22 +26,36 @@ var filesToCache = [
 	'/img/dojo/cdk.png'
 ];
 
-self.addEventListener('install', function(event) {
-	event.waitUntil(
-		caches
-			.open(cacheName)
-			.then(function(cache) {
-				return cache.addAll(filesToCache);
-			})
-	);
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
+    })
+  );
 });
 
-self.addEventListener('fetch', function(event) {
-	event.respondWith(
-		caches
-			.match(event.request)
-			.then(function(response) {
-				return response ? response : fetch(event.request);
-			})
-	);
+self.addEventListener('activate', function(e) {
+  console.log('[ServiceWorker] Activate');
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (key !== cacheName) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e) {
+  console.log('[ServiceWorker] Fetch', e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
 });
